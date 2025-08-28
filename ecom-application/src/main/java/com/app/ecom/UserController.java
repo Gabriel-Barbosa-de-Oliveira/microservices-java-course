bbd69d114bf7cfc1594a90cc726cb9a7ce445cc0
Gabriel Barbosa <gabrielbarbosaprofissional@gmail.com>
2025-08-28 00:05:36 -0300

    Feat: Return Response Entity

--- ecom-application/src/main/java/com/app/ecom/UserController.java
+++ ecom-application/src/main/java/com/app/ecom/UserController.java
@@ -1,6 +1,8 @@
 package com.app.ecom;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -14,22 +16,27 @@ public class UserController {
     private final UserService userService;
 
     @GetMapping("/api/users")
-    public List<User> getAllUsers() {
-        return userService.fetchAllUsers();
+    public ResponseEntity<List<User>> getAllUsers() {
+        return new ResponseEntity<>(userService.fetchAllUsers(), HttpStatus.OK);
+//        return ResponseEntity.ok(userService.fetchAllUsers());
     }
 
 
     @GetMapping("/api/users/{id}")
-    public User getUser(@PathVariable Long id) {
-        return userService.fetchUser(id);
+    public ResponseEntity<User> getUser(@PathVariable Long id) {
+        User user = userService.fetchUser(id);
+        if (user == null) {
+            return  ResponseEntity.notFound().build();
+        }
+        return ResponseEntity.ok(userService.fetchUser(id));
     }
 
     @PostMapping("/api/users")
-    public String createUser(@RequestBody User user) {
+    public ResponseEntity<String> createUser(@RequestBody User user) {
         userService.addUser(user);
-        return "User add successfully!";
-    }
+        return ResponseEntity.ok("User add successfully!");
 
+    }
 
 
 }
