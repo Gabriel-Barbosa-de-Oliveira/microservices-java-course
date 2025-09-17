f3ba92447cc8ce176af23f7eb558960fcef6a563
Gabriel Barbosa <gabrielbarbosaprofissional@gmail.com>
2025-09-16 22:02:05 -0300

    Adding JPA Methods

--- ecom-application/src/main/java/com/app/ecom/UserService.java
+++ ecom-application/src/main/java/com/app/ecom/UserService.java
@@ -1,5 +1,6 @@
 package com.app.ecom;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.RequestBody;
 
@@ -9,34 +10,50 @@ import java.util.Optional;
 import java.util.Random;
 
 @Service
+@RequiredArgsConstructor // Instancia Repositorios
 public class UserService {
-    private List<User> userList = new ArrayList<>();
+    private final UserRepository userRepository;
+    //    private List<User> userList = new ArrayList<>();
     private Long nextId = 1L;
 
     public List<User> fetchAllUsers() {
-        return userList;
+        return userRepository.findAll();
     }
 
     public void addUser(User user) {
-        user.setId(nextId++);
-        userList.add(user);
+//        user.setId(nextId++);
+        userRepository.save(user);
     }
 
     public Optional<User> fetchUser(Long id) {
-        return userList.stream()
-                .filter(user -> user.getId().equals(id))
-                .findFirst();
+//        return userList.stream()
+//                .filter(user -> user.getId().equals(id))
+//                .findFirst();
+
+        return userRepository.findById(id);
     }
 
     public boolean updateUser(Long id, User updatedUser) {
-        return  userList.stream()
-                .filter(user -> user.getId().equals(id))
-                .findFirst()
+//        return  userList.stream()
+//                .filter(user -> user.getId().equals(id))
+//                .findFirst()
+//                .map(existingUser -> {
+//                    existingUser.setFirstName(updatedUser.getFirstName());
+//                    existingUser.setLastName(updatedUser.getLastName());
+//                    return true;
+//                }).orElse(false);
+
+        return userRepository.findById(id)
                 .map(existingUser -> {
                     existingUser.setFirstName(updatedUser.getFirstName());
                     existingUser.setLastName(updatedUser.getLastName());
+                    userRepository.save(existingUser);
                     return true;
                 }).orElse(false);
+
     }
+
+// os metodos de findAll findById Não são declarados pois quem toma conta deles
+// é o JPA
 
 }
